795be8a26d5dad0ecf9bd417aa47da23d66a966a
Anatoliy <mazkovgg@gmail.com>
2024-09-15 09:58:24 +0300

    Initial commit

---
+++ TowerDefence/Tower.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+
+class Tower :
+    public GameObject
+{
+protected:
+    int level;
+    int cost;
+    float coocldown;
+    float cooldown_current;
+    float action_range;
+
+public:
+    Tower();
+    Tower(const Tower&);
+    virtual ~Tower();
+    virtual void Action() = 0;
+};
+
